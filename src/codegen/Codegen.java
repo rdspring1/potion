8180b343e84b3fa496190a669383a4e00345d1a7
Chad Brubaker <pencilo.mmotl@gmail.com>
2013-04-21 12:48:50 -0500

    Refactoring

--- src/codegen/Codegen.java
+++ src/codegen/Codegen.java
@@ -36,46 +36,22 @@ public class Codegen implements AstVisitor
 	{
 		//should be handled from OpDef
 	}
+
 	public void accept(OpDef def)
 	{
-		//TODO: Move this out to a common place, seriously
-		List<Tuple> node_items = new ArrayList<Tuple>();
-		List<String> node_names = new ArrayList<String>();
-		List<Tuple> edge_items = new ArrayList<Tuple>();
-		List<Attribute> attributes = new ArrayList<Attribute>();
-		for(Tuple t: def.exp.tuples){
-			for(Attribute at : t.attributes)
-				attributes.add(at);
-			switch(t.type) {
-			case NODES:
-				node_items.add(t);
-				node_names.add(get_prop_type(t,Type.Types.NODE));
-				break;
-			case EDGES:
-				edge_items.add(t);
-				break;
-			}
-		}
-		StringBuilder parambuilder = new StringBuilder(""); //for params to function calls(keep the order right)
-		for (int i=0; i<node_names.size();i++) {
-			String s = node_names.get(i);
-			parambuilder.append(s + ((i < node_names.size()-1) ? "," : " "));
-		}
-		for (int i=0; i<edge_items.size();i++) {
-			parambuilder.append(",e"+i );
-		}
+		List<Tuple> node_items = get_node_items(def.exp.tuples);
+		List<String> node_names = get_node_names(def.exp.tuples);
+		List<Tuple> edge_items = get_edge_items(def.exp.tuples);
+		List<Attribute> attributes = get_attributes(def.exp.tuples);
+		String params = get_param_string(node_names,edge_items);
 		//write our helper methods...
 		CheckShape(def);
 		CheckGuard(def);
 		Apply(def);
 
-		//TODO: generate the kernel that calls the apply above
-		//Kernel needs to handle getting the values for the apply. I'm not sure on how to do this right now
-		//Elixir paper explains it a bit but Im not sure I follow the non-slow version.. :(
-		//print variables so we have them all
-		int num_edges = 0;
 		emit("__global__ void _kernel_"+def.id.id+"(int *chaged, ...) \n{");
-		java.util.Set<String> declared = new HashSet<String>();
+		//Emit edge/node decls
+		int num_edges = 0;
 		for(Tuple t : def.exp.tuples) {
 			switch(t.type) {
 				case EDGES:
@@ -86,18 +62,20 @@ public class Codegen implements AstVisitor
 				break;
 			}
 		}
+		//default changed to false
 		emit("*changed = FALSE;");
-		//Handle our easy cases.
+		//TODO: Current supporting three types of ops, Global, 1 vertex, 2 verts with shared edge
+
 		//empty args, just call apply
 		if(def.exp.tuples.size() == 0) {
 			emit("*changed = _apply_"+def.id.id+"();");
 		} else if(node_items.size() == 1) {
 			emit("int _id = threadIdx.x;");
 			emit(node_names.get(0)+ " = _get_node(_id);");
-			emit("*changed = _apply_"+def.id.id+"("+parambuilder+");");
+			emit("*changed = _apply_"+def.id.id+"("+params+");");
 		} else if(node_items.size() == 2 && edge_items.size() == 1) {
-			emit("int id = threadIdx.x;");
-			emit(node_names.get(0)+ " = _get_node(id);");
+			emit("int _id = threadIdx.x;");
+			emit(node_names.get(0)+ " = _get_node(_id);");
 			//choose which edges we are iterating over, in edges or out edges
 			//if the first node is the src then out_edges, otherwise first node is dest and go for in_edges
 			String edge_map = (node_names.get(0).equals(get_prop_name(edge_items.get(0),"src"))) ? "out_edges" : "in_edges";
@@ -105,7 +83,7 @@ public class Codegen implements AstVisitor
 			//emit("for( std::map<int,Edge*>::iterator _it="+node_names.get(0)+"->"+edge_map+".begin(); _it != " +node_names.get(0)+"->"+edge_map+".end(); ++it) {");
 			emit("for(int _i = 0; _i < "+node_names.get(0)+"->"+edge_map+"_size ; _i++) {");
 			emit(node_names.get(1)+ " = "+node_names.get(0)+"->" + edge_map+"[_i];");
-			emit("*changed |= _apply_"+def.id.id+"("+parambuilder+");");
+			emit("*changed |= _apply_"+def.id.id+"("+params+");");
 			emit("}");
 
 		} else {
@@ -116,38 +94,19 @@ public class Codegen implements AstVisitor
 	}
 	public void Apply(OpDef def)
 	{
-		List<Tuple> node_items = new ArrayList<Tuple>();
-		List<String> node_names = new ArrayList<String>();
-		List<Tuple> edge_items = new ArrayList<Tuple>();
-		List<Attribute> attributes = new ArrayList<Attribute>();
-		for(Tuple t: def.exp.tuples){
-			for(Attribute at : t.attributes)
-				attributes.add(at);
-			switch(t.type) {
-			case NODES:
-				node_items.add(t);
-				node_names.add(get_prop_type(t,Type.Types.NODE));
-				break;
-			case EDGES:
-				edge_items.add(t);
-				break;
-			}
-		}
+
+
+		List<Tuple> node_items = get_node_items(def.exp.tuples);
+		List<String> node_names = get_node_names(def.exp.tuples);
+		List<Tuple> edge_items = get_edge_items(def.exp.tuples);
+		List<Attribute> attributes = get_attributes(def.exp.tuples);
+
 		//Generate the apply
-		StringBuilder argbuilder = new StringBuilder(""); //for the args coming in
-		StringBuilder parambuilder = new StringBuilder(""); //for params to function calls(keep the order right)
-		for (int i=0; i<node_names.size();i++) {
-			String s = node_names.get(i);
-			argbuilder.append("Node* " + s + ((i < node_names.size()-1) ? "," : ""));
-			parambuilder.append(s + ((i < node_names.size()-1) ? "," : " "));
-		}
-		for (int i=0; i<edge_items.size();i++) {
-			argbuilder.append(", Edge *e" + i);
-			parambuilder.append(",e"+i );
-		}
-		emit("__device__ inline int _apply_"+def.id.id+"("+argbuilder+")\n{")  ;
+		String params = get_param_string(node_names,edge_items);
+		String args = get_arg_string(node_names,edge_items);
+		emit("__device__ inline int _apply_"+def.id.id+"("+args+")\n{")  ;
 		emit("int _changed = FALSE;");
-		emit("if(!_checkshape_"+def.id.id+"("+parambuilder+")) return;");
+		emit("if(!_checkshape_"+def.id.id+"("+params+")) return;");
 		//Make the array of nodes for locking
 		StringBuilder nodebuilder = new StringBuilder();
 		for(String s : node_names)
@@ -161,7 +120,7 @@ public class Codegen implements AstVisitor
 		//now get all the needed attributes
 		emitGets(node_items,edge_items);
 		//guard check
-		emit("if(_checkguard_"+def.id.id+"("+parambuilder+")) {");
+		emit("if(_checkguard_"+def.id.id+"("+params+")) {");
 		for(Assignment assignment : def.exp.assignments)
 			assignment.visit(this);
 		//TODO: Do we need to emit better code than this or is passing the guard enough to set changed to true?
@@ -173,66 +132,19 @@ public class Codegen implements AstVisitor
 		emit("return _changed;");
 		emit("}\n");
 	}
-	public void emitGets(List<Tuple> node_items, List<Tuple> edge_items)
-	{
-		java.util.Set<String> declared = new HashSet<String>();
-		for(Tuple node : node_items) {
-			for(Attribute at : node.attributes) {
-				if(this.typedefs.get(at.id.id).of == Type.Types.NODE)
-					continue;
-				if(declared.contains(at.var.id))
-					continue;
-				declared.add(at.var.id);
-				emit(this.typeToString(typedefs.get(at.id.id))+" "+at.var.id);
-				emit(" = &"+ get_prop_type(node,Type.Types.NODE)+"->attribute_"+at.id.id+";\n");
-			}
-		}
-		int i=0;
-		for(Tuple edge : edge_items) {
-			String src = get_prop_name(edge,"src");
-			String dst = get_prop_name(edge,"dst");
-			for(Attribute at : edge.attributes) {
-				if(this.typedefs.get(at.id.id).of == Type.Types.NODE)
-					continue;
-				if(declared.contains(at.var.id))
-					continue;
-				declared.add(at.var.id);
-				emit(this.typeToString(typedefs.get(at.id.id))+" "+at.var.id);
-				emit(" = &e"+i+"->attribute_"+at.id.id+";\n");
-			}
-			i++;
-		}
-	}
 	public void CheckGuard(OpDef def)
 	{
 		/*TODO: This needs to go somewhere and be shared, probably in an OpDef IR node*/
-		List<Tuple> node_items = new ArrayList<Tuple>();
-		List<Tuple> edge_items = new ArrayList<Tuple>();
-		List<String> node_names = new ArrayList<String>();
-		List<Attribute> attributes = new ArrayList<Attribute>();
-		for(Tuple t: def.exp.tuples){
-			for(Attribute at : t.attributes)
-				attributes.add(at);
-			switch(t.type) {
-			case NODES:
-				node_names.add(get_prop_type(t,Type.Types.NODE));
-				node_items.add(t);
-				break;
-			case EDGES:
-				edge_items.add(t);
-				break;
-			}
-		}
-		StringBuilder argbuilder = new StringBuilder("");
 
-		for (int i=0; i<node_names.size();i++) {
-			String s = node_names.get(i);
-			argbuilder.append("Node* " + s + ((i < node_names.size()-1) ? "," : ""));
-		}
-		for (int i=0; i<edge_items.size();i++) {
-			argbuilder.append(", Edge *e" + i);
-		}
-		emit("__device__ inline int _checkguard_"+def.id.id+"("+argbuilder+")\n{");
+		List<Tuple> node_items = get_node_items(def.exp.tuples);
+		List<String> node_names = get_node_names(def.exp.tuples);
+		List<Tuple> edge_items = get_edge_items(def.exp.tuples);
+		List<Attribute> attributes = get_attributes(def.exp.tuples);
+
+		String args = get_arg_string(node_names,edge_items);
+
+
+		emit("__device__ inline int _checkguard_"+def.id.id+"("+args+")\n{");
 		emitGets(node_items,edge_items);
 		emit("return ");
 		def.exp.bexp.visit(this);
@@ -252,33 +164,13 @@ public class Codegen implements AstVisitor
 		//emit highly unoptimized code, let someone else handle that noise
 		
 		//Categorize
-		List<Tuple> node_items = new ArrayList<Tuple>();
-		List<String> node_names = new ArrayList<String>();
-		List<Tuple> edge_items = new ArrayList<Tuple>();
-		List<Attribute> attributes = new ArrayList<Attribute>();
-		for(Tuple t: def.exp.tuples){
-			for(Attribute at : t.attributes)
-				attributes.add(at);
-			switch(t.type) {
-			case NODES:
-				node_items.add(t);
-				node_names.add(get_prop_type(t,Type.Types.NODE));
-				break;
-			case EDGES:
-				edge_items.add(t);
-				break;
-			}
-		}
-		//python where are you my love :'(
-		StringBuilder argbuilder = new StringBuilder("");
-		for (int i=0; i<node_names.size();i++) {
-			String s = node_names.get(i);
-			argbuilder.append("Node* " + s + ((i < node_names.size()-1) ? "," : ""));
-		}
-		for (int i=0; i<edge_items.size();i++) {
-			argbuilder.append(", Edge *e" + i);
-		}
-		emit("__device__ inline int _checkshape_"+def.id.id+"("+argbuilder+")\n{");
+		
+		List<Tuple> node_items = get_node_items(def.exp.tuples);
+		List<String> node_names = get_node_names(def.exp.tuples);
+		List<Tuple> edge_items = get_edge_items(def.exp.tuples);
+		List<Attribute> attributes = get_attributes(def.exp.tuples);
+		String args = get_arg_string(node_names,edge_items);
+		emit("__device__ inline int _checkshape_"+def.id.id+"("+args+")\n{");
 		for (int i=0;i<node_items.size();i++) {
 			for (int j=0;j<i;j++) {
 				String ni = get_prop_type(node_items.get(i),Type.Types.NODE);
@@ -523,4 +415,106 @@ public class Codegen implements AstVisitor
 	}
 
 
+	//Helper methods for opdef, TODO:move to a util class someday
+	private List<Tuple> get_node_items(List<Tuple> tuples)
+	{
+		List<Tuple> node_items = new ArrayList<Tuple>();
+		for(Tuple t: tuples){
+			switch(t.type) {
+			case NODES:
+				node_items.add(t);
+				break;
+			}
+		}
+		return node_items;
+	}
+	private List<Tuple> get_edge_items(List<Tuple> tuples)
+	{
+		List<Tuple> edge_items = new ArrayList<Tuple>();
+		for(Tuple t: tuples){
+			switch(t.type) {
+			case EDGES:
+				edge_items.add(t);
+				break;
+			}
+		}
+		return edge_items;
+	}
+	private List<Attribute> get_attributes(List<Tuple> tuples)
+	{
+		List<Attribute> attributes = new ArrayList<Attribute>();
+		for(Tuple t: tuples){
+			for(Attribute at : t.attributes)
+				attributes.add(at);
+		}
+		return attributes;
+	}
+	private List<String> get_node_names(List<Tuple> tuples)
+	{
+		List<String> names = new ArrayList<String>();
+		for(Tuple t: tuples){
+			switch(t.type) {
+			case NODES:
+				names.add(get_prop_type(t,Type.Types.NODE));
+				break;
+			}
+		}
+		return names;
+	}
+	public String get_param_string(List<String> node_names, List<Tuple> edge_items)
+	{
+		StringBuilder parambuilder = new StringBuilder("");
+		for (int i=0; i<node_names.size();i++) {
+			String s = node_names.get(i);
+			parambuilder.append(s + ((i < node_names.size()-1) ? "," : " "));
+		}
+		for (int i=0; i<edge_items.size();i++) {
+			parambuilder.append(",e"+i );
+		}
+		return parambuilder.toString();
+	}
+
+	public String get_arg_string(List<String> node_names, List<Tuple> edge_items)
+	{
+		StringBuilder argbuilder = new StringBuilder(""); //for the args coming in
+		for (int i=0; i<node_names.size();i++) {
+			String s = node_names.get(i);
+			argbuilder.append("Node* " + s + ((i < node_names.size()-1) ? "," : ""));
+		}
+		for (int i=0; i<edge_items.size();i++) {
+			argbuilder.append(", Edge *e" + i);
+		}
+		return argbuilder.toString();
+	}
+
+	public void emitGets(List<Tuple> node_items, List<Tuple> edge_items)
+	{
+		java.util.Set<String> declared = new HashSet<String>();
+		for(Tuple node : node_items) {
+			for(Attribute at : node.attributes) {
+				if(this.typedefs.get(at.id.id).of == Type.Types.NODE)
+					continue;
+				if(declared.contains(at.var.id))
+					continue;
+				declared.add(at.var.id);
+				emit(this.typeToString(typedefs.get(at.id.id))+" "+at.var.id);
+				emit(" = &"+ get_prop_type(node,Type.Types.NODE)+"->attribute_"+at.id.id+";\n");
+			}
+		}
+		int i=0;
+		for(Tuple edge : edge_items) {
+			String src = get_prop_name(edge,"src");
+			String dst = get_prop_name(edge,"dst");
+			for(Attribute at : edge.attributes) {
+				if(this.typedefs.get(at.id.id).of == Type.Types.NODE)
+					continue;
+				if(declared.contains(at.var.id))
+					continue;
+				declared.add(at.var.id);
+				emit(this.typeToString(typedefs.get(at.id.id))+" "+at.var.id);
+				emit(" = &e"+i+"->attribute_"+at.id.id+";\n");
+			}
+			i++;
+		}
+	}
 }
