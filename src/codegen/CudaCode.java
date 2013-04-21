816008a6a3435f5e999c695ce10a2cb91abe42bb
Chad Brubaker <pencilo.mmotl@gmail.com>
2013-04-21 13:08:59 -0500

    Add more static C++ code

--- src/codegen/CudaCode.java
+++ src/codegen/CudaCode.java
@@ -9,7 +9,7 @@ public class CudaCode
 	 */
 	public static String helpers()
 	{
-		return sort();
+		return sort() + graphDecl();
 	}
 	public static String sort()
 	{
@@ -19,6 +19,43 @@ public class CudaCode
 			"{ for(int i=0;i<length;i++) for(int j=0;j<length;j++)"+
 			"    if(nodes[j]->id < nodes[i]->id) {"+
 			"      Node* tmp = nodes[i]; nodes[i] = nodes[j]; nodes[j] = tmp; }" +
-			"}";
+			"}\n";
+	}
+	public static String graphDecl()
+	{
+		return "__device__ Node * graph;\n";
+	}
+	public static String edge()
+	{
+		//For now let's make edge a noop
+		return "__device__ inline bool _edge(Node *a, Node *b) {return true;}\n";
+	}
+	public static String edge()
+	{
+		String base =
+			"class Edge {"+
+			"public:"+
+			"__device__ Node* src;"+
+			"__device__ Node* dst;";
+		//TODO: Add in attributes here
+		String from_attrs = "";
+		return base +from_attrs + "};\n"
+	}
+	public static String node()
+	{
+		String base =
+			"class Node {"+
+			"public:"+
+			"__device__ int id;"+
+			"__device__ void lock();"+
+			"__device__ void unlock();"+
+			"__device__ Edge *in_edges;"+
+			"__device__ int in_edges_size;"+
+			"__device__ Edge *out_edges;"+
+			"__device__ int out_edges_size;";
+		//TODO: this
+		String from_attrs ="";
+		String function_decls = "";
+		return base + from_attrs+"};"+function_decls+"\n";
 	}
 }
